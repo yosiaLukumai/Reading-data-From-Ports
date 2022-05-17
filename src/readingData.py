70ac4d81a0bdee1ae386058d8b7af18854858a06
unknown <yosialukumai@gmail.com>
2022-05-17 09:30:57 +0300

    modified the command

--- src/readingData.py
+++ src/readingData.py
@@ -1,5 +1,6 @@
 #importing the modules
-# Pyserial || request || schedule
+# Pyserial 
+# Author: Yosia Lukumai
 
 import serial
 import time
@@ -7,11 +8,11 @@ import time
 class ReadFromComPorts:
     comPort = ""
 
-    def __init__(self, comport: str , baudrate: int , timeout=None):
+    def __init__(self, comportname: str , baudrate: int , timeout=None):
         """
         Initialize the com port and baudrate then we create the virual port that can be shared
         """
-        self.comportname = comport
+        self.comportname = comportname
         self.baudrate = baudrate
         self.timeout = timeout
     
@@ -29,7 +30,7 @@ class ReadFromComPorts:
                 comPort = serial.Serial(self.comportname, self.baudrate, self.timeout)
                 return comPort
         except serial.serialutil.SerialException as err:
-            print("---> The port can't be found configure it :{}".format(self.comportname))
+            print("---> The port can't be found configure it make sure is  seen by your computer {}".format(self.comportname))
     
     @comPort.setter
     def comPort(self, comportname: str , baudrate:int , timeout: None):
@@ -38,7 +39,6 @@ class ReadFromComPorts:
 
         return: serial object with the port info
         """
-        print("setting the port")
         comPort = serial.Serial(comportname, baudrate, timeout)
         return comPort
     
@@ -47,15 +47,52 @@ class ReadFromComPorts:
         This will open the ports
         return: None
         """
-        print(self.comPort.baudrate)
+        try:
+            if(self.comPort.is_open):
+                print("---> Port already opened")
+            else:
+                self.comPort.open()
+        except (AttributeError, Exception ) as e:
+            print("---> Configure well your ports {}".format(self.comportname))
+
+    def checkifTheComportisOpen(self):
+        try:
+            if(self.comPort.is_open):
+                print(self.comPort.is_open)
+                print(">>>>> Port is open ready for use")
+            else:
+                print(">>>> Port is not open")
+        except Exception as e:
+            print("---> Configure your port {} well ".format(self.comportname))
 
     
     def closePort(self):
-        print(self.comPort.baudrate)
+        """
+        This will be used for closing the port
+        return: 
+        """
+        try:
+            if(self.comPort.is_open == False):
+                print("---> Port is not opened")
+            else:
+                print("here is {}".format(self.comPort.is_open))
+                return self.comPort.close()
+        except Exception as e:
+            print(">>> something has went wrong restart the script")
+        
+    
+    def readCharacters(self):
+        """
+        This  will return the character from the communication port that has been obtained after reading
+        return: 
+        Sequence: characters that has been obtained from the comp port
+        """
 
 
 
 
 
 com1 = ReadFromComPorts("COM1", 9600)
-com1.comPort
+com1.checkifTheComportisOpen()
+com1.closePort()
+com1.checkifTheComportisOpen()
